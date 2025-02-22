681f476eb89b6008b913fa6a1318e2dbeb319647
Rafael Mori <faelmori@gmail.com>
2025-02-22 22:46:11 +0300

    Refactor command structure and enhance event management functionality
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- cli/cmd/service.go
+++ cli/cmd/service.go
@@ -1,28 +1,34 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/faelmori/golife/internal"
 	"github.com/spf13/cobra"
+	"os"
+	"syscall"
 )
 
 var manager *internal.GWebLifeCycleManager
 
 func ServiceCmdList() []*cobra.Command {
-
 	return []*cobra.Command{
-		StartCmd(),
+		startCmd(),
+		stopCmd(),
+		statusCmd(),
+		restartCmd(),
+		serviceCmd(),
 	}
 }
 
-func StartCmd() *cobra.Command {
+func startCmd() *cobra.Command {
 	var processName, processCmd string
 	var processArgs []string
 	var processWait bool
 	var processEvents map[string]string
 
 	var startCmd = &cobra.Command{
-		Use:   "start",
-		Short: "Start the application",
+		Use:  "start",
+		Long: Banner + `Start the application`,
 		Run: func(cmd *cobra.Command, args []string) {
 			iManager := internal.NewLifecycleManager()
 			iManager.RegisterProcess(processName, processCmd, processArgs, processWait)
@@ -44,4 +50,80 @@ func StartCmd() *cobra.Command {
 	startCmd.Flags().StringToStringVarP(&processEvents, "events", "e", map[string]string{}, "Events to listen for and trigger")
 
 	return startCmd
+}
+func stopCmd() *cobra.Command {
+	var stopCmd = &cobra.Command{
+		Use:  "stop",
+		Long: Banner + `Stop the application`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if manager == nil {
+				return
+			} else {
+				mgr := *manager
+				mgr.StopAll()
+			}
+		},
+	}
+
+	return stopCmd
+}
+func statusCmd() *cobra.Command {
+	var statusCmd = &cobra.Command{
+		Use:  "status",
+		Long: Banner + `Check the status of the application`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if manager == nil {
+				return
+			} else {
+				mgr := *manager
+				fmt.Println(mgr.Status())
+			}
+		},
+	}
+
+	return statusCmd
+}
+func restartCmd() *cobra.Command {
+	var restartCmd = &cobra.Command{
+		Use:  "restart",
+		Long: Banner + `Restart the application`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if manager == nil {
+				return fmt.Errorf("No manager found")
+			} else {
+				mgr := *manager
+				return mgr.Restart()
+			}
+		},
+	}
+
+	return restartCmd
+}
+func serviceCmd() *cobra.Command {
+	var processName, processCmd string
+	var processArgs []string
+	var processWait bool
+	var processEvents map[string]string
+
+	var serviceCmd = &cobra.Command{
+		Use:  "service",
+		Long: Banner + `Manage the application as a service`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmdStartSpawner := fmt.Sprintf("golife service start -n %s -c %s -a %s -w %t -e %s", processName, processCmd, processArgs, processWait, processEvents)
+			cmdStartErr := syscall.Exec("/bin/sh", []string{"-c", cmdStartSpawner}, os.Environ())
+			if cmdStartErr != nil {
+				return cmdStartErr
+			} else {
+				return nil
+			}
+		},
+	}
+
+	serviceCmd.Flags().StringVarP(&processName, "name", "n", "", "Name of the process")
+	serviceCmd.Flags().StringVarP(&processCmd, "cmd", "c", "", "Command to execute")
+	serviceCmd.Flags().StringSliceVarP(&processArgs, "args", "a", []string{}, "Arguments to pass to the command")
+	serviceCmd.Flags().BoolVarP(&processWait, "wait", "w", false, "Wait for the process to finish before returning")
+	serviceCmd.Flags().StringToStringVarP(&processEvents, "events", "e", map[string]string{}, "Events to listen for and trigger")
+
+	return serviceCmd
 }
--- cli/root.go
+++ cli/root.go
@@ -5,23 +5,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const Version = "1.0.0"
-
 var rootCmd = &cobra.Command{
-	Use:     "life",
+	Use:     "golife",
 	Aliases: []string{"lfc, goLife"},
 	Example: "life start",
-	Version: Version,
-	Short:   "Programatically can be used to manage the life cycle of an application, service or module",
-	Long: `
-  #####                #                          
- #     #   ####        #        #  ######  ###### 
- #        #    #       #        #  #       #      
- #  ####  #    #       #        #  #####   #####  
- #     #  #    #       #        #  #       #      
- #     #  #    #       #        #  #       #      
-  #####    ####        #######  #  #       ###### 
-
+	Version: lcmd.Version,
+	Long: lcmd.Banner + `
 Life is a CLI tool that can be used to manage the life cycle of an application, service or module.
 It can be used to start, stop, restart, pause, resume, trigger events and more.
 With the capability to attach to a running process and listen for events, it can be used to orchestrate the life cycle of almost any application.
@@ -30,10 +19,14 @@ Happy coding! Happy Life!`,
 }
 
 func RootCmd() *cobra.Command {
-	setUsageDefinition(rootCmd)
-
 	rootCmd.AddCommand(lcmd.ServiceCmdList()...)
 	rootCmd.AddCommand(lcmd.EventsCmdList()...)
+
+	setUsageDefinition(rootCmd)
+
+	for _, cmd := range rootCmd.Commands() {
+		setUsageDefinition(cmd)
+	}
 
 	return rootCmd
 }
